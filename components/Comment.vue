8c7c48436643331eda3fc635a099b799051c9169
wiwimouse <wiwimouse@gmail.com>
2018-06-30 13:39:47 +0800

    [ADD] add comment components.

---
+++ components/Comment.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <p class="card-text">{{ body }}</p>
+    </div>
+    <div class="card-footer">
+      <a href=""
+         class="comment-author">
+        <img class="comment-author-img"
+             :src="image">
+      </a>
+      &nbsp;
+      <a href=""
+         class="comment-author">{{ username }}</a>
+      <span class="date-posted">{{ createdAt | date }}</span>
+      <span class="mod-options">
+        <i v-if="isCurrentUser"
+           class="ion-trash-a"
+           @click="$emit('delete-comment')"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: {
+    username: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    createdAt: {
+      type: String,
+      default: ''
+    },
+    body: {
+      type: String,
+      default: ''
+    },
+    currentUsername: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    isCurrentUser () {
+      return this.username === this.currentUsername
+    },
+  },
+  methods: {
+    deleteComment () {
+
+    }
+  }
+}
+</script>
+
---
+++ components/CommentForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <form class="card comment-form"
+        @submit.prevent="$emit('submit')">
+    <div class="card-block">
+      <textarea class="form-control"
+                placeholder="Write a comment..."
+                rows="3"
+                v-model="value$"></textarea>
+    </div>
+    <div class="card-footer">
+      <img class="comment-author-img"
+           :src="image">
+      <button class="btn btn-sm btn-primary"
+              :disabled="!submitable || submitting">Post Comment</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'CommentForm',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    submitting: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    value$: {
+      get () {
+        return this.value
+      },
+      set (val) {
+        this.$emit('input', val)
+      }
+    },
+    submitable () {
+      return this.value.trim().length
+    }
+  }
+}
+</script>
